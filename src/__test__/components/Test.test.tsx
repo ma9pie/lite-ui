91a01b9b3847adf9b71a952434d8d5f538ec7a55
ma9pie <ma9pie.dev@gmail.com>
2024-03-22 16:47:31 +0900

    fix : component test code

--- src/__test__/components/Test.test.tsx
+++ src/__test__/components/Test.test.tsx
@@ -1,9 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 
-import { Test } from '@/components';
-
 test('', async () => {
-  render(<Test></Test>);
-  await screen.findAllByText(/Test/);
+  render(<div>test</div>);
+  await screen.findAllByText(/test/);
 });
