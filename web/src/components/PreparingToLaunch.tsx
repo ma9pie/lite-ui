16b96e104bb47e53e64222a344889687613da228
ma9pie <ma9pie.dev@gmail.com>
2024-03-25 20:11:39 +0900

    add : temporary page

---
+++ web/src/components/PreparingToLaunch.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PreparingToLaunch = () => {
+  return (
+    <div className="preparing-to-launch">
+      Preparing To Launch Lite UI homepage 🦄
+    </div>
+  );
+};
+
+export default PreparingToLaunch;
