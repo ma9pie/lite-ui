d41a4dd8062cd245882911713cd9c57aa393880b
ma9pie <ma9pie.dev@gmail.com>
2024-04-22 17:19:17 +0900

    feat : Button component

---
+++ src/types/button.ts
@@ -0,0 +1,16 @@
+import { ReactNode } from 'react';
+
+import { DefaultProps } from '@/types';
+
+export type ButtonSize = 'sm' | 'md' | 'lg';
+export type ButtonRadius = 'none' | 'sm' | 'md' | 'lg' | 'full';
+export type ButtonColor = 'default' | 'primary';
+
+export interface ButtonProps extends DefaultProps {
+  size?: ButtonSize;
+  radius?: ButtonRadius;
+  color?: ButtonColor;
+  disabled?: boolean;
+  children?: ReactNode;
+  onClick?: (...args: any) => void;
+}
