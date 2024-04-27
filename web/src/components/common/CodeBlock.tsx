4def5524a5878d209d257ea97e35108ccb0fc0d0
ma9pie <ma9pie.dev@gmail.com>
2024-04-27 19:55:24 +0900

    feat : CodeBlock copy

--- web/src/components/common/CodeBlock.tsx
+++ web/src/components/common/CodeBlock.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { FaCheck, FaRegCopy } from 'react-icons/fa6';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { vs2015 } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 import tw, { styled } from 'twin.macro';
@@ -13,16 +15,44 @@ interface Props {
   code?: string;
 }
 
-const CodeBlock = ({ className, code = '' }: Props) => {
+const CodeBlock = ({ code = '', ...props }: Props) => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const copyCode = () => {
+    setIsCopied(true);
+  };
+
   return (
-    <Wrapper className={className} language="typescript" style={vs2015}>
-      {code}
+    <Wrapper {...props}>
+      <SyntaxHighlighter
+        language="typescript"
+        style={vs2015}
+        customStyle={{ padding: 0, backgroundColor: 'transparent' }}
+      >
+        {code}
+      </SyntaxHighlighter>
+
+      <CopyToClipboard text={code} onCopy={copyCode}>
+        <IconWrapper isCopied={isCopied}>
+          {isCopied ? (
+            <FaCheck size={20}></FaCheck>
+          ) : (
+            <FaRegCopy size={20}></FaRegCopy>
+          )}
+        </IconWrapper>
+      </CopyToClipboard>
     </Wrapper>
   );
 };
 
 export default CodeBlock;
 
-const Wrapper = styled(SyntaxHighlighter)`
-  ${tw`rounded-lg !p-4`};
+const Wrapper = styled.div`
+  ${tw`relative p-4 rounded-xl bg-neutral-800`};
+`;
+const IconWrapper = styled.div<{ isCopied: boolean }>`
+  ${tw`absolute top-2 right-2 p-2 rounded-lg transition-colors duration-200 cursor-pointer`};
+  ${tw`text-neutral-500`};
+  ${tw`hover:(text-neutral-200 bg-neutral-600)`};
+  ${({ isCopied }) => isCopied && tw`text-neutral-200`};
 `;
