1301b3f6001909ad0f28a09f65fa62030032ab64
ma9pie <ma9pie.dev@gmail.com>
2024-05-02 13:23:24 +0900

    feat : Code size, full, hideIcon props

--- src/types/code.ts
+++ src/types/code.ts
@@ -1,5 +1,9 @@
 import { DefaultProps } from '@/types';
 
+export type CodeSize = 'sm' | 'md' | 'lg';
 export interface CodeProps extends DefaultProps {
-  code?: string;
+  size?: CodeSize;
+  code: string;
+  full?: boolean;
+  hideIcon?: boolean;
 }
