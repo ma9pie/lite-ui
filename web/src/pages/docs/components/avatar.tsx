be65c9befaec3b05c63e0b360b50ff5312c48b08
ma9pie <ma9pie.dev@gmail.com>
2024-05-05 19:41:24 +0900

    docs : Avatar

--- web/src/pages/docs/components/avatar.tsx
+++ web/src/pages/docs/components/avatar.tsx
@@ -2,46 +2,28 @@ import React from 'react';
 
 import DocsLayout from '@/components/layouts/DocsLayout';
 import DocsForm from '@/components/pages/docs/DocsForm';
-import AlertIcon from '@/components/pages/docs/exmaples/alert/AlertIcon';
-import AlertStatus from '@/components/pages/docs/exmaples/alert/AlertStatus';
+import AvatarSize from '@/components/pages/docs/exmaples/avatar/AvatarSize';
 
 const DocsComponentPage = () => {
   return (
     <DocsLayout>
       <DocsForm
-        name="Alert"
-        description="Display messages that require attention."
-        examples={[
-          { title: 'Status', component: AlertStatus },
-          { title: 'Icon', component: AlertIcon },
-        ]}
+        name="Avatar"
+        description="Used to represent user's profile, symbol image or icon."
+        examples={[{ title: 'Size', component: AvatarSize }]}
         apiData={[
           {
             key: '0',
-            property: 'status',
-            type: ['success', 'info', 'warn', 'error'],
-            description: 'Type of Alert styles',
-            defaultValue: 'success',
+            property: 'size',
+            type: ['xs', 'sm', 'md', 'lg', 'xl'],
+            description: 'Size of Avatar',
+            defaultValue: 'md',
           },
           {
             key: '1',
-            property: 'iconSize',
-            type: ['number'],
-            description: 'Size of icon',
-            defaultValue: 20,
-          },
-          {
-            key: '2',
-            property: 'hideIcon',
-            type: ['boolean'],
-            description: 'Hide icon',
-            defaultValue: false,
-          },
-          {
-            key: '3',
-            property: 'children',
-            type: ['ReactNode'],
-            description: 'Alert content',
+            property: 'src',
+            type: ['string'],
+            description: 'Image source',
           },
         ]}
       ></DocsForm>
