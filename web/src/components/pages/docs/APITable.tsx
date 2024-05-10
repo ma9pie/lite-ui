5f6ac5811f18e0781bea1335419920162e069d68
ma9pie <ma9pie.dev@gmail.com>
2024-05-10 16:07:21 +0900

    design: APITable list padding y

--- web/src/components/pages/docs/APITable.tsx
+++ web/src/components/pages/docs/APITable.tsx
@@ -1,5 +1,6 @@
 import { Table } from '@ma9pie/lite-ui';
 import React from 'react';
+import tw, { styled } from 'twin.macro';
 
 import { APITableRowItem } from '@/types';
 
@@ -13,19 +14,31 @@ const APITable = ({ rows }: Props) => {
       className="text-sm"
       minWidth={600}
       field={[
-        { key: 'property', value: 'Property', flex: 2 },
+        {
+          key: 'property',
+          value: 'Property',
+          flex: 2,
+          render: ({ property }) => <Item>{property}</Item>,
+        },
         {
           key: 'type',
           value: 'Type',
           flex: 3,
-          render: ({ type }) => type.join(' | '),
+          render: ({ type }) => <Item>{type.join(' | ')}</Item>,
         },
-        { key: 'description', value: 'Description', flex: 3 },
+        {
+          key: 'description',
+          value: 'Description',
+          flex: 3,
+          render: ({ description }) => <Item>{description}</Item>,
+        },
         {
           key: 'defaultValue',
           value: 'Default',
           flex: 2,
-          render: ({ defaultValue }) => JSON.stringify(defaultValue) || '-',
+          render: ({ defaultValue }) => (
+            <Item>{JSON.stringify(defaultValue) || '-'}</Item>
+          ),
         },
       ]}
       rows={rows.map((item, idx) => ({ key: idx, ...item }))}
@@ -34,3 +47,7 @@ const APITable = ({ rows }: Props) => {
 };
 
 export default APITable;
+
+const Item = styled.div`
+  ${tw`py-1`};
+`;
