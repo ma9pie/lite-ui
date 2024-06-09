1217fb7f09440e112e1ccf8b1e6547f5fccc36f3
ma9pie <ma9pie.dev@gmail.com>
2024-06-09 18:54:54 +0900

    config : edit jest config (add babel)

--- jest.config.js
+++ jest.config.js
@@ -1,29 +1,13 @@
-const babelConfigEmotion = {
-  presets: [
-    [
-      'next/babel',
-      {
-        'preset-react': {
-          runtime: 'automatic',
-          importSource: '@emotion/react',
-        },
-      },
-    ],
-  ],
-  plugins: [
-    require.resolve('babel-plugin-macros'),
-    require.resolve('@emotion/babel-plugin'),
-  ],
-};
-
 const customJestConfig = {
+  preset: 'ts-jest',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
+  testMatch: ['<rootDir>/src/**/*.test.(js|jsx|ts|tsx)'],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx|mjs)$': ['babel-jest', babelConfigEmotion],
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest'],
   },
 };
 
